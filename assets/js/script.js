13071aed93ca6fd842029d1b9db6858e4403772a
Michael <mbahl1670@gmail.com>
2021-11-18 20:53:52 -0600

    created getMeme function, which does a fetch from the Giphy API, returns 5 values and console logs them

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,46 @@ var getWord = function(word) {
 
     fetch(startWord).then(function (response) {
         response.json().then(function(data) {
+            if (data.noun) {
+                var num = data.noun.syn.length;
+                var pickRandomSynNum = randomNumber(0,num);
+                var wordToMeme = data.noun.syn[pickRandomSynNum];
+
+                getMeme(wordToMeme);
+            }
+           
+        });
+    });
+}
+
+var getMeme = function(wordFromThesaurus) {
+    // returning 5 values from the giphy API
+    var giphyURL = "https://api.giphy.com/v1/gifs/search?api_key=vcTR1GucFAwcW13jdyTEqRNcYzBbE9E2&q=" + wordFromThesaurus + "&limit=5&offset=0&rating=r&lang=en";
+
+    fetch(giphyURL).then(function (response) {
+        response.json().then(function(data) {
             console.log(data);
         });
     });
 }
 
+// functionality stuff to make it look prettier. 
+// when you click the text area the placeholder clears,
+// if you click off the text area the placeholder reappears
+$("#memeWord").on("click", function() {
+    $("#memeWord").attr("placeholder", "").val("");
+}).on("blur", function() {
+    $("#memeWord").attr("placeholder", "Type a noun")
+});
+
 $("#memeBtn").on("click", function(event) {
     event.preventDefault();
     var newWord = $("#memeWord").val().trim();
     getWord(newWord);
+    $("#memeWord").val(""); // clears the text area after we click the button
 })
+
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+    return value;
+  };
