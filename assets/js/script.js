e63a839dccd784e819b9bd55d86862e02c57906b
Shaun S <shaun.sticka@outlook.com>
2021-11-19 20:34:21 -0600

    For help erase after

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// memeKey = "vcTR1GucFAwcW13jdyTEqRNcYzBbE9E2";
+
 // var startWord = "https://words.bighugelabs.com/api/2/9a06618119fb219174cc6aaec15b4f46/love/json";
 
 var getWord = function(word) {
@@ -16,3 +18,32 @@ $("#memeBtn").on("click", function(event) {
     getWord(newWord);
 })
 
+
+// start giphy stuff below here feel free to move as needed
+
+var getGiph = function(meme) {
+    var memeURL = "https://api.giphy.com/v1/gifs/search?api_key=vcTR1GucFAwcW13jdyTEqRNcYzBbE9E2&limit=5&offset=0&rating=pg-13&lang=en&q=" + meme;
+
+    fetch(memeURL).then(function (response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+
+
+
+$("#giphybtn").on("click", function(event) {
+    event.preventDefault();
+    var newMeme = $("#giphyWord").val().trim();
+    getGiph(newMeme);
+    console.log(event.data[0].id);
+});
+
+// might return something other than a gif. Suggest only returning gifs. 
+// type is located at content.data[i].type
+
+// display downsized gif
+
+var downEx = document.createElement("img");
+// downEx.src = 
