55d9f74bf970dfb62a9ee1c513cd4368b1097fda
Michael <mbahl1670@gmail.com>
2021-11-25 13:22:22 -0600

    ready to delete

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,39 +14,6 @@ var randomNumber = function(min, max) {
 var getWord = function(word) {
     var startWord = "https://words.bighugelabs.com/api/2/9a06618119fb219174cc6aaec15b4f46/" + word +"/json";
 
-<<<<<<< HEAD
-    for (let run = 0; run < 3; run++) {
-        
-    
-
-        fetch(startWord).then(function (response) {
-            response.json().then(function(data) {
-                if (data.noun) { // If (“noun” in objectName)
-                    console.log(data);
-                    var num = data.noun.syn.length - 1; // num = the number of possible synonyms a word has
-                    var pickRandomSynNum = randomNumber(0,num); // returns a random number representing a random word in the noun.array
-                    var wordToMeme = data.noun.syn[pickRandomSynNum]; // sets the word that we will meme to a random synonym.
-                    
-                    if (!word || !wordToMeme) {
-                        alert("Something went wrong!");
-                        return;
-                    }
-
-                    // I think this function should be outside this function call - first it makes it easier to use in future, second, we might need to include get word in a loop and we don't want it logging every synonym it pulls - just the original search
-                    // save the original word and the synonym to local storage
-                    
-                    // addToMemeHistory(word, wordToMeme); // adds the orignal word + the synonym word to the search history bar
-                    getMeme(wordToMeme); // passes the random synonym into a function that will make a call to the giphy API
-                    
-                    var newWord = isNew(word);
-                    console.log(newWord);
-                    
-                    if (newWord) {
-                        addToMemeHistory(word, wordToMeme);
-                        var newHistoryToAdd =  {
-                            memeWord: word,
-                            memeSyn: wordToMeme
-=======
 
     fetch(startWord).then(function (response) {
         if (response.ok) {
@@ -97,20 +64,10 @@ var getWord = function(word) {
                             memeSyn1: wordToMeme1,
                             memeSyn2: wordToMeme2,
                             memeSyn3: wordToMeme3
->>>>>>> feature/debugging
                         };
                         searchHistory.push(newHistoryToAdd);
                         localStorage.setItem("history", JSON.stringify(searchHistory));
                     }
-<<<<<<< HEAD
-                    
-
-                }
-            
-            });
-        });
-    };
-=======
 
                 } else {
                     alert("Please try another word.");
@@ -126,7 +83,6 @@ var getWord = function(word) {
     .catch(function(error) {
         alert("Unable to connect to Big Thesaurus!");
     });
->>>>>>> feature/debugging
 };
 
 var getMeme = function(wordFromThesaurus) {
@@ -134,12 +90,6 @@ var getMeme = function(wordFromThesaurus) {
     var giphyURL = "https://api.giphy.com/v1/gifs/search?api_key=vcTR1GucFAwcW13jdyTEqRNcYzBbE9E2&q=" + wordFromThesaurus + "&limit=1&offset=0&rating=r&lang=en";
 
     fetch(giphyURL).then(function (response) {
-<<<<<<< HEAD
-        response.json().then(function(data) {
-
-            showThatApp(data, wordFromThesaurus); // calls the function that will display info onto the page
-        });
-=======
         if (response.ok) { 
             response.json().then(function(data) {
                 // console.log(data);
@@ -155,7 +105,6 @@ var getMeme = function(wordFromThesaurus) {
     })
     .catch(function(error) {
         alert("Unable to connect to Giphy!")
->>>>>>> feature/debugging
     });
 }
 
